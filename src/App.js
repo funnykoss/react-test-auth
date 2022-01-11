c81eed2587b5bf7d2f7c4caf2ccf493b7070ec1c
funnykoss <75586141+funnykoss@users.noreply.github.com>
2022-01-11 08:12:27 +0200

    clean

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import React from "react";
-// import { Routes, Route } from "react-router-dom";
 import { authOperations, authSelectors } from "./redux/auth";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Switch } from "react-router-dom";
-import PrivateRoute from "./components/PrivateRoute";
-// import PublicRoute from "./components/PublicRoute";
-
-// import LoginView from "./views/LoginView/LoginView";
 import RegisterView from "./views/RegisterView/RegisterView";
 import HelloPage from "./views/HelloPage/HelloPage";
 
