df90bc75a6a7e3867939605ba257f5251a9690b0
funnykoss <75586141+funnykoss@users.noreply.github.com>
2022-01-11 03:09:04 +0200

    fix action

--- src/redux/auth/auth-slice.jsx
+++ src/redux/auth/auth-slice.jsx
@@ -10,7 +10,7 @@ const authSlice = createSlice({
   name: 'auth',
   initialState,
   extraReducers: {
-    [authOperations.register.fulfilled](state, {action}) {
+    [authOperations.register.fulfilled](state, action) {
       state.user = action.payload.user;
       state.token = action.payload.token;
       state.isLoggedIn = true;
